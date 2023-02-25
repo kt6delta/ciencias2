55c9c7fa976f2aae22722aacc9254b43437b9d28
kt6delta <lcpreciadob@correo.udistrital.edu.co>
2023-02-25 03:26:41 -0500

    8000 registros y time

--- AgendaRN/Main.java
+++ AgendaRN/Main.java
@@ -3,29 +3,30 @@ import java.util.Scanner;
 
 import AgendaRN.Logic.Datos;
 import AgendaRN.Logic.*;
+import java.util.Random;
 
 public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Arbol Arbolito = new Arbol();
+        Random random = new Random();
         System.out.println("Agenda de contactos");
         System.out.println("Ingrese la cantidad de contactos a agendar: ");
         int n = sc.nextInt();
-        Arbol Arbolito = new Arbol();
         Datos datos[]= new Datos[n];
 
+        long time_i = System.nanoTime(); //tiempo inicial
         for (int i = 0; i < n; i++) {
-          System.out.println("Ingrese nombre: ");
-          String nombre = sc.next();
-          System.out.println("Ingrese apellido: ");
-          String apellido = sc.next();
-          System.out.println("Ingrese ciudad: ");
-          String ciudad = sc.next();
+          String nombre = (char) (random.nextInt(26) + 'a')+""+(char) (random.nextInt(26) + 'a')+(char) (random.nextInt(26) + 'a');
+          String apellido = (char) (random.nextInt(26) + 'a')+""+(char) (random.nextInt(26) + 'a')+(char) (random.nextInt(26) + 'a');
+          String ciudad = (char) (random.nextInt(26) + 'a')+""+(char) (random.nextInt(26) + 'a')+(char) (random.nextInt(26) + 'a');
           datos[i]=new Datos(i,nombre, apellido, ciudad);
           Arbolito.insertar(datos[i]);
         }
         //Arbolito.eliminar(datos[n-1]);
         //System.out.println(Arbolito.buscar(datos[n-1]));
         Arbolito.recorrer(Recorrido.POSTORDER);//Left, Right, Nodo
-
+        long double_f = (System.nanoTime() - time_i)/1000000;
+        System.out.println("tiempo de ejecucion: " + double_f+" mili segundos");
     }
 }
